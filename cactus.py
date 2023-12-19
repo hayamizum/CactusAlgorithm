4f813e95cfa142049bd9754d29b2630d6c8edacc
Keita Watanabe <56672415+keita1126@users.noreply.github.com>
2023-12-19 15:23:27 +0900

    Add files via upload

--- cactus.py
+++ cactus.py
@@ -1,7 +1,7 @@
 #Constructing Cactus
 #
 #written by Keita Watanabe, Waseda university
-#2023.11.9
+#2023.12.19
 #
 #
 import sys
@@ -11,29 +11,59 @@ import matplotlib.pyplot as plt
 import itertools
 import copy
 import csv
+import pandas as pd
 
-#Function readcsv
-#Input data from csv file
+#関数 readcsv
 def readcsv(filename):
     distance_matrix = []
     with open(filename, 'r', newline='', encoding='utf-8-sig') as file:
         reader = csv.reader(file)
         for i, row in enumerate(reader):
             if i == 0:
-                #First row is size of matrix
+                # 1行目は行列の大きさの情報
                 n = int(row[0])
                 continue
-            #Add the data from the second line to a distance matrix
+            # 2行目以降のデータを数値としてリストに追加します
             distance_matrix.append([float(value) for value in row])
     return n, np.array(distance_matrix)
 
-#Function comp
-#Calculate compactification index in V
+#関数 add_csv .csvを付ける
+def add_csv(filename):
+    return filename if filename.endswith('.csv') else f"{filename}.csv"
+
+#関数 non_negative
+def non_negative(value):
+    return max(value, 0)
+
+#深さ優先探索
+def DFS(graph, v, visited, component):
+    visited[v] = True
+    component.append(v)
+    for i in range(len(graph)):
+        if graph[v][i] == 1 and not visited[i]:
+            DFS(graph, i, visited, component)
+
+#連結成分
+def connected_components(graph):
+    num_vertices = len(graph)
+    visited = [False] * num_vertices
+    components = []
+
+    for v in range(num_vertices):
+        if not visited[v]:
+            component = []
+            DFS(graph, v, visited, component)
+            components.append(component)
+
+    return components
+
+
+#関数comp Compactification indexをV内で計算する．
 def comp(V, x):
 
     global D
     
-    #Calculate minimum value
+    #x以外の要素をy,zに初期値として割り当てる
     if x==list(V)[0]:
         y,z = list(V)[1], list(V)[2]
         min_val = (D[x,y]+D[x,z]-D[y,z])/2
@@ -52,11 +82,11 @@ def comp(V, x):
         if val < min_val:
             min_val = val
             min_comb = (y, z)
-    
-    return min_val, min_comb    
 
-##Function Compactification (Tightening)
-#Determine how much each vertex "extends" from the set of other vertices using tightening (Compactification).
+    
+    return non_negative(round(min_val,5)) , min_comb
+
+#関数Compactification
 def Compactification(V):
     global D
     global VertexNum
@@ -66,7 +96,8 @@ def Compactification(V):
     global Adj_opt
     V_aux = set()
 
-    print(V)
+    Adj = np.zeros((len(Adj),len(Adj)))
+
     if len(V)<=2:
         mkAdj(V)
         return
@@ -79,88 +110,64 @@ def Compactification(V):
         if index == 0:
             continue
         else:
-            #Auxiliary vertex
+            #追加頂点の情報
             VertexNum += 1
             aux = VertexNum
-            #Expand distance matrix
+            #距離行列拡張
             D = np.append(D, np.zeros((1, D.shape[0])), axis=0)
             D = np.append(D, np.zeros((D.shape[0], 1)), axis=1)
-            #Write x, y, z into the distance matrix
+            #距離行列書き込み(まずcompに関わったもの)
             D[x,aux] = D[aux,x] = index
-            D[y,aux] = D[aux,y] = D[y,x] - D[x,aux]
-            D[z,aux] = D[aux,z] = D[z,x] - D[x,aux]
-            #Identification
+            D[y,aux] = D[aux,y] = non_negative(round(D[y,x] - D[x,aux],5))
+            D[z,aux] = D[aux,z] = non_negative(round(D[z,x] - D[x,aux],5))
+            #同一視
             isEqualed = 0
             for a in {x,y,z}:
                 if D[a, aux] == 0:
-                    if a in V_del:
-                        doEnd = 1
                     VertexNum -= 1
                     D = np.delete(D, aux, axis=0)
                     D = np.delete(D, aux, axis=1)
                     isEqualed = 1
                     break
             if isEqualed == 1:
-                continue
-            #Write other verticies into the distance matrix
+                continue#この後はやらなくてよい
+            #距離行列書き込み(compに関わってないもの)
             for a in AllVertices.union(V_aux)-{x,y,z,aux}:
-                D[a,aux] = D[aux,a] = max(D[a,t] - D[t,aux] for t in {x,y,z})
-                #Identification
-                if D[a, aux] == 0:
-                    if a in V_del:
-                        doEnd = 1
-                    VertexNum -= 1
-                    D = np.delete(D, aux, axis=0)
-                    D = np.delete(D, aux, axis=1)
-                    isEqualed = 1
-                    break
+                D[a,aux] = D[aux,a] = non_negative(round(max(D[a,t] - D[t,aux] for t in {x,y,z}),5))
+                #同一視
+                if a in V.union(V_aux):
+                    if D[a, aux] == 0:
+                        VertexNum -= 1
+                        D = np.delete(D, aux, axis=0)
+                        D = np.delete(D, aux, axis=1)
+                        isEqualed = 1
+                        break
             if isEqualed == 0:
                 V_aux.add(aux)
                 AllVertices.add(aux)
 
-    if VertexNum_tmp==VertexNum and doEnd==1:
-        for x,y in itertools.permutations(V,2):
-            if Adj[x,y]==1:
-                Adj[x,y] = 0
-                Adj_opt[x,y] = 1
-        V_del = V_del.union(V)
-        return
-    
     mkAdj(V.union(V_aux))
 
 
 
-#Function CountNeighbor
-#Count neighbor of x
-def CountNeighbor(x):
-    global Adj
+#関数CountNeighbor
+def CountNeighbor(x, Adj):
     NeighborNum = 0
     for i in range(len(Adj)):
         if Adj[x,i]==1:
             NeighborNum += 1
     return NeighborNum
 
-
-#Function CountOptNeighbor
-#Count neighbor of x in optimal realization
-def CountOptNeighbor(x):
-    global Adj_opt
-    NeighborNum = 0
-    for i in range(len(Adj_opt)):
-        if Adj_opt[x,i]==1:
-            NeighborNum += 1
-    return NeighborNum
-
-
-#Function makeAdjacent
+#関数makeAdjacent
 def mkAdj(V):
     global D
     global V_del
     global AllVertices
     global Adj
     global Adj_opt
+    global Adj_opt_tmp
 
-    #Expand adjacent matrix
+    #隣接行列拡張
     for x in V:
         if x >= len(Adj):
             k_start = len(Adj)
@@ -169,44 +176,45 @@ def mkAdj(V):
                 Adj = np.append(Adj, np.zeros((Adj.shape[0], 1)), axis=1)
                 Adj_opt = np.append(Adj_opt, np.zeros((1, Adj_opt.shape[0])), axis=0)
                 Adj_opt = np.append(Adj_opt, np.zeros((Adj_opt.shape[0], 1)), axis=1)
+                Adj_opt_tmp = np.append(Adj_opt_tmp, np.zeros((1, Adj_opt_tmp.shape[0])), axis=0)
+                Adj_opt_tmp = np.append(Adj_opt_tmp, np.zeros((Adj_opt_tmp.shape[0], 1)), axis=1)
         
-    for x, y in itertools.permutations(V,2):#Regard x and y as adjacent
+    for x, y in itertools.permutations(V,2): #完全グラフの隣接関係をいれる
         Adj[x,y] = 1
-        for z in AllVertices-{x,y}: #Remove the edges that turns out to be non-adjacent
-            if D[x,z]+D[z,y] == D[x,y]:
+        for z in V-{x,y}: #隣接していないものを切る
+            if D[x,z]+D[z,y] == D[x,y] and D[x,z] != 0 and D[z,y] != 0:
                 Adj[x,y] = 0
 
 
     isDeleted = 0
 
     for x in V:
-        print(x,CountNeighbor(x))
-        if CountNeighbor(x)==0:
+        if CountNeighbor(x, Adj)==0:
             isDeleted = 1
             V_del.add(x)
-        elif CountNeighbor(x)==1:
+        elif CountNeighbor(x, Adj)==1:
             isDeleted = 1
             for i in range(len(Adj)):
                 if Adj[x,i]==1:
                     u = i
             Adj_opt[x,u] = Adj_opt[u,x] = 1
             V_del.add(x)
-        elif CountNeighbor(x)==2:
+        elif CountNeighbor(x, Adj)==2:
             isDeleted = 1
             Neighbor = set()
             for i in range(len(Adj)):
                 if Adj[x,i]==1:
                     Neighbor.add(i)
-            if comp(Neighbor,x)[0] == 0:
-                u = Neighbor.pop()
-                v = Neighbor.pop()
-                Adj_opt[x,u] = Adj_opt[u,x] = 1
-                Adj_opt[x,v] = Adj_opt[v,x] = 1
-                V_del.add(x)
+            u = Neighbor.pop()
+            v = Neighbor.pop()
+            Adj_opt[x,u] = Adj_opt[u,x] = 1
+            Adj_opt[x,v] = Adj_opt[v,x] = 1
+            V_del.add(x)
 
     for x in V_del:
         for y in V.union(V_del):
             Adj[x,y] = Adj[y,x] = 0
+
 
     if isDeleted==0:
         for x,y in itertools.permutations(V.difference(V_del),2):
@@ -216,73 +224,114 @@ def mkAdj(V):
         V_del = V_del.union(V)
         return
 
+
+    components = connected_components(Adj)
     
-    if len(V.difference(V_del))==1:
-        x = V.difference(V_del).pop()
-        V_del.add(x)
+    for connect in components:
+        connect = set(connect)
+        if len(connect)==1:
+            x = connect.pop()
+            V_del.add(x)
     
-    if len(V.difference(V_del))==2:
-        x,y = V.difference(V_del)
-        if Adj[x,y]==1:
-            Adj[x,y] = 0
-            Adj_opt[x,y] = 1
-        V_del = V_del.union({x,y})
+        if len(connect)==2:
+            x,y = connect
+            if Adj[x,y]==1:
+                Adj[x,y] = 0
+                Adj_opt[x,y] = 1
+            V_del = V_del.union({x,y})
 
-    if len(V.difference(V_del))>2:
-        Compactification(V.difference(V_del))
+        if len(connect)>2:
+            Compactification(connect)
 
 
-#Function Slack
-#Find all verticies
+#関数SetDelete
+def SetDelete(V, delete):
+    V_list = list(V.difference(delete))
+    V_list.sort()
+    for x in V_list:
+        V_list[V_list.index(x)] = V_list.index(x)
+    return set(V_list)
+
+
+#関数Slack
 def Slack(V):
     global D
     global V_del
     global AllVertices
     global Slack_del
+    global Adj
+    global Adj_opt
+    global Adj_opt_tmp
+    global VertexNum
+    global loop
     
     V_tmp = copy.deepcopy(V)
+    Adj_opt_tmp = copy.deepcopy(Adj_opt)
+
+    loop = loop + 1
+    Set_of_Neighbor = set()
 
     for x in V_tmp:
-        if CountOptNeighbor(x)<=2:
+        if CountNeighbor(x, Adj_opt_tmp)<=2:
             Slack_del.add(x)
-        elif CountOptNeighbor(x)>=3:
-            Slack_del.add(x)
+            continue
+        elif CountNeighbor(x, Adj_opt_tmp)>=3:
             Neighbor = set()
-            for i in range(len(Adj)):
-                if Adj_opt[x,i]==1:
+            for i in range(len(Adj_opt_tmp)):
+                if Adj_opt_tmp[x,i]==1:
                     Neighbor.add(i)
-            V_del = set()
             Neighbor.add(x)
-            for x,y in itertools.permutations(Neighbor,2):
-                Adj[x,y] = 1
-                Adj_opt[x,y] = 0
-            Compactification(Neighbor)
+            Slack_del.add(x)
+            Set_of_Neighbor.add(frozenset(Neighbor))
+            for y,z in itertools.permutations(Neighbor,2):
+                Adj_opt[y,z] = 0
 
-    if len(AllVertices.difference(Slack_del))==1:
-        x = AllVertices.difference(Slack_del).pop()
-        Slack_del.add(x)
+    for Neighbor in Set_of_Neighbor:
+        Compactification(Neighbor)
+
+    Delete_set = set()
+    for x,y in itertools.combinations(AllVertices,2):
+        if D[x,y]==0:
+            Delete_set.add(max(x,y))
+            for i in range(len(Adj_opt)):
+                if i==min(x,y):
+                    Adj_opt[min(x,y),i] = 0
+                else:
+                    Adj_opt[min(x,y),i] = Adj_opt[x,i] or Adj_opt[y,i]
+                    Adj_opt[i,min(x,y)] = Adj_opt[i,x] or Adj_opt[i,y]
+
+    Delete_list = list(Delete_set)
+
+    D = np.delete(D, Delete_list, axis=0)
+    D = np.delete(D, Delete_list, axis=1)
+    Adj = np.delete(Adj, Delete_list, axis=0)
+    Adj = np.delete(Adj, Delete_list, axis=1)
+    Adj_opt = np.delete(Adj_opt, Delete_list, axis=0)
+    Adj_opt = np.delete(Adj_opt, Delete_list, axis=1)
+
+    VertexNum = VertexNum - len(Delete_list)
+    AllVertices = SetDelete(AllVertices, Delete_list)
+    V = SetDelete(V, Delete_list)
+    V_tmp = SetDelete(V_tmp, Delete_list)
+    V_del = SetDelete(V_del, Delete_list)
+    Slack_del = SetDelete(Slack_del, Delete_list)
     
-    if len(AllVertices.difference(Slack_del))==2:
-        x,y = AllVertices.difference(Slack_del)
-        if Adj[x,y]==1:
-            Adj[x,y] = 0
-            Adj_opt[x,y] = 1
-        Slack_del = Slack_del.union({x,y})
-
-    if len(AllVertices.difference(Slack_del))>2:
+    if loop==1:
+        Slack(AllVertices)
+    elif len(AllVertices.difference(Slack_del))>0:
         Slack(AllVertices.difference(Slack_del))
 
 
 
 #Input
 mode = input("csv or stdin: ")
-if mode == "csv":#csv mode
+if mode == "csv" or mode == "c":#ファイル名を標準入力から入力してデータをリストに格納
     filename = input("File Name: ")
-    n, data_list = readcsv(filename)
+    n, data_list = readcsv(add_csv(filename))
     D = np.array(data_list)
-elif mode == "stdin":#standard input mode
+elif mode == "stdin" or mode == "s":#標準入力
     print("Input n:",end=" ")
-    n = int(input()) #Input size of distance matrix
+    n = int(input()) #行数の入力
     print("Input Distance Matrix: ")
     D_list = [list(map(float, input().split())) for i in range(n)] #2行目以降標準入力
     D = np.array(D_list)
@@ -294,26 +343,57 @@ else:
 X = set(range(n))
 VertexNum = n-1
 
-#Construction
-Adj = np.array([[0]])
-Adj_opt = np.array([[0]])
+#構成
+Adj = np.ones((n,n))
+np.fill_diagonal(Adj, 0)
+Adj_opt = np.ones((n,n))
+np.fill_diagonal(Adj_opt, 0)
+Adj_opt_tmp = np.ones((n,n))
+np.fill_diagonal(Adj_opt_tmp, 0)
 V_del = set()
 AllVertices = copy.deepcopy(X)
-Compactification(X)
-
-#Slack
 Slack_del = set()
-Slack(AllVertices)
+loop = 0
+Slack(X)
 
-#Define the graph
+
+#部分的構築により要らない辺がある場合があるため要らない辺を切る
+for x, y in itertools.permutations(AllVertices,2): #完全グラフの隣接関係をいれる
+    Adj_opt[x,y] = 1
+    for z in AllVertices-{x,y}: #隣接していないものを切る
+        if D[x,z]+D[z,y] == D[x,y] and D[x,z] != 0 and D[z,y] != 0:
+            Adj_opt[x,y] = 0
+
+#Graphの定義
 G = nx.Graph(Adj_opt)
 for x,y in itertools.permutations(AllVertices,2):
     if Adj_opt[x][y]==1:
-        G[x][y]['weight'] = D[x,y]
+        G[x][y]['weight'] = round(D[x,y],5)
 
-#Output the graph
 edge_labels = nx.get_edge_attributes(G, 'weight')
-#If considering weighted plotting, remove the lower three lines
+
+
+#元データとあっているか
+# ラベルを0からの順序で作成する
+labels = list(range(len(G)))
+# 頂点間の全てのペアに対して最短距離を計算します．
+distances = dict(nx.all_pairs_dijkstra_path_length(G, weight='weight'))
+# 距離行列（PandasのDataFrame）に変換します．
+distance_matrix = pd.DataFrame(distances).transpose().fillna(float('inf'))
+# ラベルの並び順に距離行列を並び替えます．
+distance_matrix = distance_matrix[labels].loc[labels]
+#npに変換
+distance_matrix_np = distance_matrix.values
+# numpyのarrayに変換されたdistance_matrixをnxnにトリミング
+distance_matrix_trimmed = distance_matrix_np[:n, :n]
+D_loc = D[:n, :n]
+# distance_matrixとDを比較
+if np.array_equal(np.round(distance_matrix_trimmed,5), np.round(D_loc,5)):
+    print("True")
+else:
+    print("False")
+
+#重みを考慮した配置にしたい場合は以下の三行をコメントアウト
 for x,y in itertools.permutations(AllVertices,2):
     if Adj_opt[x][y]==1:
         G[x][y]['weight'] = 1
