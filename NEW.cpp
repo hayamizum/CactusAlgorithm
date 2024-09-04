2faaff053d9e9d56446b656fabb6e0d42d136bdb
Yuhao Zhang <31284538+YuhaoZhangwow@users.noreply.github.com>
2024-09-04 11:13:21 -0700

    Update NEW.cpp

--- NEW.cpp
+++ NEW.cpp
@@ -12,6 +12,8 @@ double SimilarityParameter=0.1;
 bool Adjacent[1000][1000];
 int n,n_Auxilliary=0;
 vector<int>Now_Graph;
+void test_weight();
+void test_dis();
 
 void CSV_READ() {
     ifstream file(file_name.c_str());
@@ -59,7 +61,10 @@ void DistanceMatrixReadIn(){
         cout << "Please enter " << n << "x" << n << " numbers below\ninput here:\n";
 
         int COUNT = n * n;
-        while (COUNT--) cin >> DistanceMatrix[COUNT / n][COUNT % n];
+        
+        for(int i=0;i<COUNT;i++){
+        	cin >> DistanceMatrix[i / n][i % n];
+		}
 
         cout << "Completed! Start calculating...\n";
     } else {
@@ -90,30 +95,71 @@ void FloydWarshall(){
     }
     
 	for(int i=0;i<n;i++)
-		for(int j=0;j<n;j++)
-			Weight[i][j]=Weight[j][i]=DistanceMatrix[i][j];
+		for(int j=0;j<n;j++){
+			if(i!=j)
+				Weight[i][j]=Weight[j][i]=DistanceMatrix[i][j];
+			else
+				Weight[i][j]=Weight[j][i]=INT_MAX;
+		}
 }
 
-void Compactification(int v,double cx){
+double Max3(int x,int y,int z,int i,int r){
+	double ans=-1;
+	ans=max(ans,DistanceMatrix[x][i]-DistanceMatrix[x][r]);
+	ans=max(ans,DistanceMatrix[y][i]-DistanceMatrix[y][r]);
+	ans=max(ans,DistanceMatrix[z][i]-DistanceMatrix[z][r]);
+	return ans;
+}
+
+void Compactification(int v,double cx,int y,int z){
 	n_Auxilliary++;
 	int new_vertex=n-1+n_Auxilliary;
 	Now_Graph.push_back(new_vertex);
+	Weight[new_vertex][new_vertex]=INT_MAX;
 	
-	DistanceMatrix[v][new_vertex]=DistanceMatrix[new_vertex][v]=cx;
-	Weight[v][new_vertex]=Weight[new_vertex][v]=cx;
+	DistanceMatrix[v][new_vertex]=DistanceMatrix[new_vertex][v]=cx;//calculate the distance(r,x)
+	Weight[v][new_vertex]=Weight[new_vertex][v]=cx;//build the w(r,x)
+	//precalculate the distance(r,y) and distance(r,z)
+	DistanceMatrix[y][new_vertex]=DistanceMatrix[new_vertex][y]=DistanceMatrix[v][y]-cx;
+	DistanceMatrix[z][new_vertex]=DistanceMatrix[new_vertex][z]=DistanceMatrix[v][z]-cx;
 	
 	for(int i=0;i<new_vertex;i++){
 		
 		if(i==v)continue;
-		
-		DistanceMatrix[i][new_vertex]=DistanceMatrix[new_vertex][i]=DistanceMatrix[i][v]-cx;
-		Weight[new_vertex][i]=Weight[i][new_vertex]=INT_MAX;
-		
-		if(Weight[v][i]!=INT_MAX){
+		if(Weight[v][i]!=INT_MAX){//if it is neighbor
 			
-			Weight[new_vertex][i]=Weight[i][new_vertex]=Weight[v][i]-cx;
-			Weight[v][i]=Weight[i][v]=INT_MAX;
+			if(i!=y&&i!=z)//prevent redundant calculation
+				DistanceMatrix[i][new_vertex]=DistanceMatrix[new_vertex][i]=DistanceMatrix[i][v]-cx;
+			Weight[new_vertex][i]=Weight[i][new_vertex]=Weight[v][i]-cx;//build new edges
+			Weight[v][i]=Weight[i][v]=INT_MAX;//cut the existed edges
 			
+		}
+		else{// if it is not neighbor
+			
+			if(i!=y&&i!=z)
+				DistanceMatrix[i][new_vertex]=DistanceMatrix[new_vertex][i]=Max3(v,y,z,i,new_vertex);
+			Weight[new_vertex][i]=Weight[i][new_vertex]=INT_MAX;
+			
+		}
+	}
+}
+
+void Clear_Redundant_vertex(){
+	for(int i=0;i<Now_Graph.size();i++){
+		int vertex_i=Now_Graph[i];
+		for(int j=i+1;j<Now_Graph.size();j++){
+			int vertex_j=Now_Graph[j];
+			if(DistanceMatrix[vertex_i][vertex_j]==0){
+//				test_weight();
+//				cout<<"distance 0 are pair->"<<vertex_i<<" and "<<vertex_j<<'\n';
+				for(int k=0;k<n+n_Auxilliary;k++){
+					if(Weight[vertex_j][k]!=INT_MAX){
+						Weight[vertex_i][k]=Weight[k][vertex_i]=Weight[k][vertex_j];
+						Weight[vertex_j][k]=Weight[k][vertex_j]=INT_MAX;
+					}
+				}
+//				test_weight();
+			}
 		}
 	}
 }
@@ -121,6 +167,7 @@ void Compactification(int v,double cx){
 void Check_Compactification(){
 	int Size=Now_Graph.size();
 	for(int i=0;i<Size;i++){
+		int y,z;
 		int vertex_i=Now_Graph[i];
 		double CurrentCx=INT_MAX;
 		for(int j=0;j<Size;j++){
@@ -134,14 +181,19 @@ void Check_Compactification(){
 				if(Cx==0){
 					goto early_ending;
 				}
-				CurrentCx=min(CurrentCx,Cx);
+				if(Cx<CurrentCx){
+					CurrentCx=Cx;
+					y=vertex_j;
+					z=vertex_k;
+				}
 			}
 		}
-		Compactification(vertex_i,CurrentCx);
+		Compactification(vertex_i,CurrentCx,y,z);
 		early_ending:
 			while(0);
 			//ending
 	}
+	Clear_Redundant_vertex();
 }
 
 int get_degree(int v){
@@ -191,15 +243,20 @@ void Clear_Graph_Vertex(){
 }
 
 void test_weight(){
+	cout<<"Test Weight Matrix:\n";
 	for(int i=0;i<n+n_Auxilliary;i++){
 		for(int j=0;j<n+n_Auxilliary;j++){
-			cout<<Weight[i][j]<<" ";
+			if(Weight[i][j]!=INT_MAX)
+				cout<<Weight[i][j]<<" ";
+			else
+				cout<<'x'<<" ";
 		}
 		cout<<endl;
 	}
 }
 
 void test_dis(){
+	cout<<"Test Distance Matrix:\n";
 	for(int i=0;i<n+n_Auxilliary;i++){
 		for(int j=0;j<n+n_Auxilliary;j++){
 			cout<<DistanceMatrix[i][j]<<" ";
@@ -208,17 +265,11 @@ void test_dis(){
 	}
 }
 
-void Algorithm(){
-	Check_Compactification();
-	Clear_Graph_Edge();
-	Clear_Graph_Vertex();
-}
-
 void Print_Graph(){
 	
 	for(int i=0;i<n+n_Auxilliary;i++){
 		for(int j=0;j<n+n_Auxilliary;j++){
-			if(Weight[i][j]!=INT_MAX&&Weight[i][j]!=0)
+			if(Weight[i][j]!=INT_MAX)
 				cout<<Weight[i][j]<<" ";
 			else
 				cout<<'x'<<" ";
@@ -226,6 +277,20 @@ void Print_Graph(){
 		cout<<endl;
 	}
 	
+}
+
+void Algorithm(){
+	Check_Compactification();
+	Clear_Graph_Edge();
+	Clear_Graph_Vertex();
+	
+	Check_Compactification();
+	Clear_Graph_Edge();
+	Clear_Graph_Vertex();
+	
+	Check_Compactification();
+	Clear_Graph_Edge();
+	Clear_Graph_Vertex();
 }
 
 int main() {
@@ -239,3 +304,21 @@ int main() {
     
     return 0;
 }
+/*
+x 1 x 1 1 x x x
+1 x 1 x x 1 x x
+x 1 x 1 x x 1 x
+1 x 1 x x x x 1
+1 x x x x 1 x 1
+x 1 x x 1 x 1 x
+x x 1 x x 1 x 1
+x x x 1 1 x 1 x
+*/
+
+/*
+0 3 6 5 7
+3 0 3 6 4
+6 3 0 3 1
+5 6 3 0 4
+7 4 1 4 0
+*/
